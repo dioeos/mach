5c589a0bef6016f4f22a00250286563bb7c1479d
Ryan Dioneda <er.dioneda@gmail.com>
2025-06-23 15:30:38 -0400

    Implemented JSON reading + writing (defaults) functionality

--- mach/src/main.rs
+++ mach/src/main.rs
@@ -1,11 +1,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod file;
+
 use global_hotkey::{
     hotkey::{Code, HotKey, Modifiers},
     GlobalHotKeyEvent, GlobalHotKeyManager, HotKeyState,
 };
 use slint::{invoke_from_event_loop, CloseRequestResponse, PlatformError};
-use std::{error::Error, thread};
+use std::{
+    error::Error,
+    io::{self, ErrorKind},
+    path::PathBuf,
+    thread,
+};
 
 slint::include_modules!();
 
@@ -14,6 +21,31 @@ fn main() -> Result<(), PlatformError> {
     let wind = ui.window(); //window componenet
     let weak_wind = ui.as_weak();
     let weak_wind2 = ui.as_weak();
+
+    let macro_file = "macros.json";
+
+    let file_path: PathBuf =
+        file::get_config_file(macro_file).map_err(|e| PlatformError::from(e.to_string()))?;
+
+    let macros: Vec<file::Macros> = match file::read_macros(&file_path) {
+        Ok(m) => {
+            println!("Received");
+            m
+        }
+        Err(e) if e.kind() == ErrorKind::NotFound => {
+            println!("Not found, defaults");
+            let defaults = file::write_default_macros(&file_path)
+                .map_err(|e| PlatformError::from(e.to_string()))?;
+            defaults
+        }
+        Err(e) => {
+            return Err(PlatformError::from(format!("I/O error: {}", e)));
+        }
+    };
+
+    for m in &macros {
+        println!("{} -> {}", m.keys, m.action);
+    }
 
     wind.on_close_requested(move || {
         if let Some(w) = weak_wind.upgrade() {
@@ -53,13 +85,13 @@ fn main() -> Result<(), PlatformError> {
         }
     });
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+    // ui.on_request_increase_value({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         ui.set_counter(ui.get_counter() + 1);
+    //     }
+    // });
 
     slint::run_event_loop_until_quit()
 }
